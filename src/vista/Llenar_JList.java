b6376c8e8c4321473e113499e20dbf94e2dc6ca9
taniacacuango <tania.cacuango329ist17dejulio.edu.ec>
2024-12-21 01:14:49 -0500

    JLIST

---
+++ src/vista/Llenar_JList.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author ruby
+ */
+public class Llenar_JList {
+    public static void main(String[] args) {
+    Jlist i = new JList();
+    i.setVisible() 
+    
+  }
+ 
+}
